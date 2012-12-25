f71d8776f7a1c14deb38c7fa3364f7a7b2cb02a6
Albert Wu <albert12132@gmail.com>
2012-12-24 16:35:11 -0800

    added database wrapper

---
+++ databases/db-mongo.js
@@ -0,0 +1,40 @@
+
+var MongoClient = require('mongodb').MongoClient;
+
+var host = 'localhost';
+var port = '27017';
+var dbname = 'questionDb';
+
+var path = 'mongodb://' + host + ':' + port + '/' + dbname;
+
+exports.getAll = function(dbname, callback) {
+  MongoClient.connect(path, function(err, db) {
+    if (err) return console.log(err);
+    db.collection(dbname)
+      .find()
+      .toArray(function(err, result) {
+        callback(err, result);
+        db.close();
+      });
+  });
+};
+
+exports.get = function(dbname, query, callback) {
+  MongoClient.connect(path, function(err, db) {
+    if (err) return console.log(err);
+    db.collection(dbname).findOne(query, function(err, result) {
+      callback(err, result);
+      db.close();
+    });
+  });
+};
+
+exports.save = function(dbname, doc, callback) {
+  MongoClient.connect(path, function(err, db) {
+    if (err) console.dir(err);
+    db.collection(dbname).save(doc, function() {
+      callback();
+      db.close();
+    });
+  });
+};
