207e24c89c0e8070b94394e866c0923ad9ffa1b7
Sharad Vikram <sharad.vikram@gmail.com>
2012-12-24 18:23:11 -0800

    added basic Procfile

--- routes/question.js
+++ routes/question.js
@@ -4,6 +4,7 @@ var db = require('../databases/db-mongo.js');
 /* GET */
 exports.list = function (req, res) {
     db.getAll('questions', function(err, result) {
+        if (err) throw err;
         res.render('qlist', {list: result});
     });
 };
@@ -11,6 +12,7 @@ exports.list = function (req, res) {
 exports.single = function (req, res) {
     var query = {title: req.params.name};
     db.get('questions', query, function(err, result) {
+        if (err) throw err;
         res.render('question', {
             title: result.title,
             body: result.body,
