43ce5b98ee19fc1a89aab936cb4837afa66eaf3e
Sharad Vikram <sharad.vikram@gmail.com>
2012-12-24 21:55:29 -0800

    added pytutor code

--- util/model.js
+++ util/model.js
@@ -1,10 +1,26 @@
 var model = {
-    Code : {
-        Output : function(heap, globals, stdout) {
-            this.heap = heap;
-            this.globals = globals;
-            this.stdout = stdout;
-        }
-    }
+  /**
+   * The Output class is used to encapsulate output from Online Python Tutor
+   */
+  Output : function(heap, globals, stdout) {
+    this.heap = heap;
+    this.globals = globals;
+    this.stdout = stdout;
+  },
+
+  /**
+   * The Question class encapsulates the questions that are in the database
+   */
+  Question : function(id, title, content, solution, tests, category, tags, type, comments) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.solution = solution;
+    this.tests = tests;
+    this.category = category;
+    this.tags = tags;
+    this.type = type;
+    this.comments = comments;
+  }
 }
 module.exports = model;
--- util/pytutor.js
+++ util/pytutor.js
@@ -1,7 +1,7 @@
-var rest = require('restler'),
-    querystring = require('querystring'),
-    model = require('./model.js'),
-    URL = require('url');
+var rest = require('restler')
+  , querystring = require('querystring')
+  , model = require('./model.js')
+  , URL = require('url');
 
 var url = "http://www.pythontutor.com/web_exec_py3.py";
 
@@ -10,53 +10,19 @@ exports.run = function(code, callback) {
     cumulative_mode : false,
     user_script : code
   }
-  rest.get(url + "?" + querystring.stringify(data), {
-  }).on('complete', function(result) {
+  rest.get(url + "?" + querystring.stringify(data)).on('complete', function(result) {
     if (result instanceof Error) {
-        callback(result);
-        return;
+      callback(result);
+      return;
     }
     trace = JSON.parse(result).trace;
     if (typeof(callback) == "function") {
-        last = trace[trace.length - 1];
-        output = new model.Code.Output(last.heap, last.globals, last.stdout);
-        callback(output);
+      last = trace[trace.length - 1];
+      output = new model.Output(last.heap, last.globals, last.stdout);
+      callback(output);
     }
   });
 }
 exports.compare = function(sub, sol) {
-    for (var i in sub.globals) {
-        if (typeof(sub.globals[i]) != typeof(sol.globals[i])) {
-            return false;
-        }
-        if (typeof(sub.globals[i]) == "object") {
-            if (sub.globals[i] && sol.globals[i]) {
-                if (sub.globals[i][0] == "REF") {
-
-                    subpointer = sub.globals[i][1]+"";
-                    solpointer = sol.globals[i][1]+"";
-
-                    if (JSON.stringify(sub.heap[subpointer]) != JSON.stringify(sol.heap[solpointer])) {
-                        return false;
-                    }
-                }
-            }
-        } else if (sub.globals[i] != sol.globals[i]) {
-            return false;
-        }
-    }
-    return true;
+  return sub.stdout.trim() == sol.stdout.trim();
 }
-exports.run("x=[1,2,3]\nprint(x)", function(result) {
-    if (result instanceof Error) {
-        return;
-    }
-    console.log(result);
-    exports.run("x=[3,2,1]", function(res) {
-        if (res instanceof Error) {
-            return;
-        }
-        console.log(res);
-        console.log(exports.compare(result, res));
-    });
-})
