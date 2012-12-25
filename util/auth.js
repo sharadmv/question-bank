0cd6284d0d998f0a157cf249db9f30924b526899
Sharad Vikram <sharad.vikram@gmail.com>
2012-12-25 14:32:01 -0800

    added basic auth

---
+++ util/auth.js
@@ -0,0 +1,18 @@
+var Connection= require('ssh2');
+
+exports.login = function(username, password, callback) {
+  var ssh = new Connection();
+  ssh.on('ready', function() {
+    callback(true);
+    ssh.end();
+  });
+  ssh.on('error', function(err) {
+    callback(false);
+  });
+  ssh.connect({
+    host: 'cory.eecs.berkeley.edu',
+    port: 22,
+    username: username,
+    password : password
+  });
+}
